fc1d394b139aaa52c526f8fb4c02a94ff866d7cd
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2024-12-20 23:01:50 +0000

    added 'pass' to incomplete function

--- options-pricer/volatility_calculators.py
+++ options-pricer/volatility_calculators.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 from assets import Asset
+import api_connection
 
 class VolatilityCalculator(ABC):
     @abstractmethod
     def calculate_volatility(self, asset: Asset) -> float: pass
+
+class SimpleVolatilityCalculator(VolatilityCalculator):
+    def calculate_volatility(self, asset: Asset) -> float:
+        pass
     
 class HistoricalVolatilityCalculator(VolatilityCalculator):
     def calculate_volatility(self, asset: Asset) -> float:
