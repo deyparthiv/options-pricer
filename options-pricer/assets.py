b876c5f8a9f90eb51daf02079b9e70cc3e6581a6
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2024-12-21 13:32:11 +0000

    implemented get_volatility for assets

--- options-pricer/assets.py
+++ options-pricer/assets.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 import api_connection
+import numpy 
+import volatility_calculators
 
 class Asset(ABC):
     @abstractmethod
@@ -9,6 +11,7 @@ class Asset(ABC):
     @abstractmethod
     def get_interest_rate(self) -> float: pass
 
+# TO DO : add dividend yield
 class Stock(Asset):
     def __init__(self, symbol: str):
         self.symbol = symbol
@@ -16,6 +19,6 @@ class Stock(Asset):
     def get_spot_price(self) -> float:
         return api_connection.get_spotprice_of_stock(self.symbol)
     def get_volatility(self) -> float:
-        pass
+        return volatility_calculators.SimpleVolatilityCalculator().calculate_volatility(self)
     def get_interest_rate(self) -> float:
         return api_connection.get_risk_free_interest_rate()
