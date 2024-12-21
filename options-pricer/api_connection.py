b7884659d2e148b85990ad10344eab32f7564194
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2024-12-21 11:15:54 +0000

    finished simply volatiity calculator function and api function to get historical price data

--- options-pricer/api_connection.py
+++ options-pricer/api_connection.py
@@ -4,11 +4,25 @@ from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
 
-def get_historical_price_of_stock(symbol:str):
+"""returns a list of historical prices of a stock for the purposes of volatility calcultaion.
+    The prices are the closing prices every day for the last five years. 
+    Args:
+        symbol (str): the symbol of the stock
+    Returns:
+        list: a list of historical prices of the stock
+    Raises:
+        Exception: error fetching data
+"""
+def get_historical_price_of_stock(symbol:str) -> list:
     print(symbol)
     financial_modelling_prep_api_key : str= os.getenv("FINANCIAL_MODELLING_PREP_API_KEY")
     url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}?apikey={financial_modelling_prep_api_key}"
     response = requests.get(url)
     if response.status_code != 200:
         raise Exception("Error fetching data")
-    return response.json()
+    values = response.json()
+    values = values['historical']
+    historial_prices = []
+    for value in values:
+        historial_prices.append(value['close'])
+    return historial_prices
--- options-pricer/volatility_calculators.py
+++ options-pricer/volatility_calculators.py
@@ -1,14 +1,21 @@
 from abc import ABC, abstractmethod
 from assets import Asset
 import api_connection
+import numpy as np
 
 class VolatilityCalculator(ABC):
     @abstractmethod
     def calculate_volatility(self, asset: Asset) -> float: pass
-
+    
 class SimpleVolatilityCalculator(VolatilityCalculator):
     def calculate_volatility(self, asset: Asset) -> float:
-        pass
+        """Returns the volatility of a stock based on the historical prices of the stock
+        Args:
+            asset (Asset): the asset whose volatility is to be calculated
+        Returns: float: the standard derivation of the historical prices of the stock"""
+        historical_prices = api_connection.get_historical_price_of_stock(asset.symbol)
+        volalitity = np.std(historical_prices)
+        return volalitity
     
 class HistoricalVolatilityCalculator(VolatilityCalculator):
     def calculate_volatility(self, asset: Asset) -> float:
