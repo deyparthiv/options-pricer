91e02a002abd9ce4eefa791cbb4950e41baec842
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2025-01-02 17:13:24 +0000

    fixed docstring

--- options_pricer/api_connection.py
+++ options_pricer/api_connection.py
@@ -1,18 +1,19 @@
-import requests
-import os 
-from dotenv import load_dotenv
 from datetime import date
+import os
+import requests
+from dotenv import load_dotenv
+
 
 load_dotenv() #load the environment variables from the .env file
 financial_modelling_prep_api_key = os.getenv("FINANCIAL_MODELLING_PREP_API_KEY")
 
-"""returns the historical end of day prices of a stock from a given date"""
 def get_historical_prices_of_stock_from_date(symbol:str, start_date: date) -> list:
+    """Returns the historical end of day prices of a stock from a given date."""
     date_string= start_date.strftime("%Y-%m-%d")
     url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}?from={date_string}?apikey={financial_modelling_prep_api_key}"
     response = requests.get(url)
     if response.status_code != 200:
-        raise Exception(f"Error fetching historical prices for {symbol}")
+        raise requests.RequestException(f"Error fetching historical prices for {symbol}")
     values = response.json()
     values = values['historical']
     historial_prices = []
@@ -21,13 +22,12 @@ def get_historical_prices_of_stock_from_date(symbol:str, start_date: date) -> li
     print("Succesfully fetched historical price for",symbol)
     return historial_prices
 
-"""returns the historical end of day prices of a stock for the last 5 years"""
 def get_historical_prices_of_stock(symbol:str) -> list:
-    #daily end of day prices for the last 5 years
+    """Returns the historical end of day prices of a stock from the last 5 years."""
     url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}?apikey={financial_modelling_prep_api_key}"
     response = requests.get(url)
     if response.status_code != 200:
-        raise Exception(f"Error fetching historical prices for {symbol}")
+        raise requests.RequestException(f"Error fetching historical prices for {symbol}")
     values = response.json()
     values = values['historical']
     historial_prices = []
@@ -36,14 +36,13 @@ def get_historical_prices_of_stock(symbol:str) -> list:
     print("Succesfully fetched historical price for",symbol)
     return historial_prices
 
-"""returns the spot price if a stock"""
 def get_spotprice_of_stock(symbol:str) -> float:
+    """Returns the spot price of a stock."""
     url = f"https://financialmodelingprep.com/api/v3/quote-short/{symbol}?apikey={financial_modelling_prep_api_key}"
     response = requests.get(url)
     if response.status_code != 200:
-        raise Exception(f"Error fetching spot price for {symbol}")
+        raise requests.RequestException(f"Error fetching spot price for {symbol}")
     value = response.json()
     print("Succesfully fetched spot price for",symbol)
     return value[0]['price']
-"""returns the interest rate right now"""
 
--- options_pricer/risk_free_interest_calculator.py
+++ options_pricer/risk_free_interest_calculator.py
@@ -1,6 +1,5 @@
-
-"""at the time of writing this code, the risk free interest rate of 4.36% is estimated from
+def get_risk_free_interest_rate() -> float:
+    """at the time of writing this code, the risk free interest rate of 4.36% is estimated from
 the yield of a 10 year UK government bond. 
 """
-def get_risk_free_interest_rate() -> float:
     return 0.0436
